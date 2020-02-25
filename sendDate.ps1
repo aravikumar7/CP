382a5b68fb6576e8de7ccaaa7af9e45dd87d3c70
aravikumar7 <aravikumar7@gmail.com>
2020-02-25 12:11:46 +0000

    Add files via upload

---
+++ sendDate.ps1
@@ -0,0 +1,13 @@
+﻿$wshell = New-Object -ComObject wscript.shell;
+$wshell.AppActivate('Quote and buy - Landing Page')
+Sleep 1
+$wshell.SendKeys(12)
+Sleep 1
+$wshell.SendKeys({TAB})
+Sleep 1
+$wshell.SendKeys(10)
+Sleep 1
+$wshell.SendKeys({TAB})
+Sleep 1
+$wshell.SendKeys(2018)
+Sleep 1
